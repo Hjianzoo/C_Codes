e589d6793c6d53b2166c1dd881dcaa456a5bda1a
hjianzoo <1161999782@qq.com>
2018-06-30 09:00:01 +0800

    add crc16

---
+++ CRC16/crc16_check.h
@@ -0,0 +1,12 @@
+/*
+ * crc16_check.h
+ *
+ *  Created on: 2018年1月16日
+ *      Author: Ethan
+ */
+
+#ifndef APPLICATION_WIFI_UART_CRC16_CHECK_H_
+#define APPLICATION_WIFI_UART_CRC16_CHECK_H_
+
+unsigned short CRC16_Check(unsigned char *puchMsg, unsigned short usDataLen);
+#endif /* APPLICATION_WIFI_UART_CRC16_CHECK_H_ */
--- calc/framework.c
+++ calc/framework.c
@@ -3,5 +3,4 @@
 int calculator(const struct calc *p)
 {
     return p->p(p->a,p->b);
-  // return 1;
 }
